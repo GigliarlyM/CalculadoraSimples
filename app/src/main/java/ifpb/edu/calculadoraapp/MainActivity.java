6f603486a99f4b4c43cb91490f2378c3dc8c9d49
Gigliárly Marcelino <60317202+GigliarlyM@users.noreply.github.com>
2022-03-31 12:56:38 -0300

    com-4-botoes

--- app/src/main/java/ifpb/edu/calculadoraapp/MainActivity.java
+++ app/src/main/java/ifpb/edu/calculadoraapp/MainActivity.java
@@ -13,39 +13,113 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText2;
     private TextView textView;
     private Button btnSoma;
+    private Button btnDivide;
+    private Button btnMultiplica;
+    private Button btnSubtrai;
     private float resultadoSoma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
-        editText = findViewById(R.id.number1);
-        editText2 = findViewById(R.id.number2);
-        textView = findViewById(R.id.resultado);
-        btnSoma = findViewById(R.id.btn_soma);
+        editText = (EditText) findViewById(R.id.number1);
+        editText2 = (EditText) findViewById(R.id.number2);
+        textView = (TextView) findViewById(R.id.resultado);
+        btnSoma = (Button) findViewById(R.id.btn_soma);
+        btnDivide = (Button) findViewById(R.id.btn_dividir);
+        btnMultiplica = (Button) findViewById(R.id.btn_multiplicar);
+        btnSubtrai = (Button) findViewById(R.id.btn_subtrair);
 
-        btnSoma.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float num1 = 0;
-                float num2 = 0;
-
-
-                num1 = Float.valueOf(String.valueOf(editText.getText()));
-                num2 = Float.valueOf(String.valueOf(editText2.getText()));
-
-                calcNumeros(num1, num2);
-
-            }
-        });
+        btnSoma.setOnClickListener(new CalculoSoma());
+        btnDivide.setOnClickListener(new CalculoDivisao());
+        btnMultiplica.setOnClickListener(new CalculoMultiplicao());
+        btnSubtrai.setOnClickListener(new CalculoSubtracao());
     }
 
-    public void calcNumeros(float num1, float num2){
-        this.resultadoSoma = num1 + num2;
-        String resultadoString = String.valueOf(this.resultadoSoma);
+    class CalculoSoma implements View.OnClickListener {
 
-        textView.setText(resultadoString);
+        private float resultadoSoma;
+
+        @Override
+        public void onClick(View view) {
+            float num1 = 0;
+            float num2 = 0;
+            String resultadoString;
+
+            num1 = Float.valueOf(String.valueOf(editText.getText()));
+            num2 = Float.valueOf(String.valueOf(editText2.getText()));
+
+            this.resultadoSoma = num1 + num2;
+            resultadoString = String.valueOf(this.resultadoSoma);
+
+            textView.setText(resultadoString);
+        }
+
+    }
+
+    class CalculoDivisao implements View.OnClickListener {
+
+        private float resultadoSoma;
+
+        @Override
+        public void onClick(View view) {
+            float num1 = 0;
+            float num2 = 0;
+
+
+            num1 = Float.valueOf(String.valueOf(editText.getText()));
+            num2 = Float.valueOf(String.valueOf(editText2.getText()));
+
+            this.resultadoSoma = num1 / num2;
+            String resultadoString = String.valueOf(this.resultadoSoma);
+
+            textView.setText(resultadoString);
+        }
+
+    }
+
+    class CalculoMultiplicao implements View.OnClickListener {
+
+        private float resultadoSoma;
+
+        @Override
+        public void onClick(View view) {
+            float num1 = 0;
+            float num2 = 0;
+
+
+            num1 = Float.valueOf(String.valueOf(editText.getText()));
+            num2 = Float.valueOf(String.valueOf(editText2.getText()));
+
+            this.resultadoSoma = num1 * num2;
+            String resultadoString = String.valueOf(this.resultadoSoma);
+
+            textView.setText(resultadoString);
+        }
+
+    }
+
+    class CalculoSubtracao implements View.OnClickListener {
+
+        private float resultadoSoma;
+
+        @Override
+        public void onClick(View view) {
+            float num1 = 0;
+            float num2 = 0;
+
+
+            num1 = Float.valueOf(String.valueOf(editText.getText()));
+            num2 = Float.valueOf(String.valueOf(editText2.getText()));
+
+            this.resultadoSoma = num1 - num2;
+            String resultadoString = String.valueOf(this.resultadoSoma);
+
+            textView.setText(resultadoString);
+        }
+
     }
 
 }
